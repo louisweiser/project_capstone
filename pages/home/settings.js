a1f824567d30b3978454c083bffa1ed8fadaf382
Louis Weiser <louis.weiser98@gmail.com>
2023-03-22 15:31:19 +0100

    implement settingpage

---
+++ pages/home/settings.js
@@ -0,0 +1,9 @@
+import Header from "@/components/common/Heading/HomeNavigation.js";
+
+export default function SettingPage() {
+  return (
+    <>
+      <Header title="Settings"></Header>
+    </>
+  );
+}
