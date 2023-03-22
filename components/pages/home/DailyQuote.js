c02ff90b3df4b8b789a337ae7ded727a6afb14bd
Louis Weiser <louis.weiser98@gmail.com>
2023-03-22 10:27:03 +0100

    add dailyquote component for home

---
+++ components/pages/home/DailyQuote.js
@@ -0,0 +1,14 @@
+import styles from "./dailyquote.module.css";
+
+export default function DailyQuote() {
+  return (
+    <div className={styles.display}>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit esse
+        error dolore inventore nihil pariatur. Deserunt enim repudiandae odio,
+        quia consequatur a sequi consectetur quidem distinctio voluptatum
+        doloremque neque. Nostrum!
+      </h4>
+    </div>
+  );
+}
