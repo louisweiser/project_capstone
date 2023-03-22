5c7fc2dbea614c175e759111fe602aa5275cd2da
Louis Weiser <louis.weiser98@gmail.com>
2023-03-22 15:30:48 +0100

    implement dictonarypage

---
+++ pages/home/dictonary.js
@@ -0,0 +1,11 @@
+import Header from "@/components/common/Heading/HomeNavigation.js";
+import Dictonary from "@/components/pages/home/Dictonary.js";
+
+export default function DictonaryPage() {
+  return (
+    <>
+      <Header title="Dictonary"></Header>
+      <Dictonary></Dictonary>
+    </>
+  );
+}
