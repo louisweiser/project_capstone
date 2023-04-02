99aee751ad655943bb90a9426b26498b9d511021
Louis Weiser <louis.weiser98@gmail.com>
2023-04-02 10:27:26 +0200

    delete for user unnecessary header

--- components/common/Heading/Home.js
+++ components/common/Heading/Home.js
@@ -1,13 +1,8 @@
-import SettingButton from "@/components/common/Link/Setting.js";
-
 import styles from "./home.module.css";
 
 export default function Header({ title }) {
   return (
     <header className={styles.header}>
-      <div className={styles.position}>
-        <SettingButton></SettingButton>
-      </div>
       <h1>{title}</h1>
     </header>
   );
