3e06dd4c52163f62df6fbc2143f61ad071bc73cb
Louis Weiser <louis.weiser98@gmail.com>
2023-04-03 11:16:07 +0200

    implement global data provider in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import "@/assets/fonts.css";
 
+import { DataProvider } from "../contexts/dataContext.js";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+  );
 }
