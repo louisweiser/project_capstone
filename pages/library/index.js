3b3e80ba8da08944c6498c114bd7f9d72ff54954
Louis Weiser <louis.weiser98@gmail.com>
2023-04-03 11:18:30 +0200

    create genre data

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,9 +1,11 @@
 import Background from "@/components/common/Background/blue.js";
+import CollectionFromData from "@/components/pages/library/CollectionData.js";
 
 export default function LibraryPage() {
   return (
     <>
       <Background></Background>
+      <CollectionFromData></CollectionFromData>
     </>
   );
 }
---
+++ public/data/genre.js
@@ -0,0 +1,9 @@
+//import { genreData } from "@/public/data/genre.js";
+
+export const genreData = [
+  "Fantasy",
+  "Non-Fiction",
+  "Philosophy",
+  "Fiction",
+  "Science-Fiction",
+];
