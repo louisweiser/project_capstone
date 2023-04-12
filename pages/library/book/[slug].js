471fb153eae236d5ff29f3ea137db5666917389c
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 11:44:30 +0200

    create book detail slug page

---
+++ pages/library/book/[slug].js
@@ -0,0 +1,11 @@
+import Header from "@/components/common/Heading/DetailPage.js";
+import BookDetails from "@/components/pages/library/BookDetails.js";
+
+export default function BookDetailPage() {
+  return (
+    <>
+      <Header></Header>
+      <BookDetails></BookDetails>
+    </>
+  );
+}
