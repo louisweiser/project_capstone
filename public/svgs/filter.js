0a621e807c6c4f5ef46735192040c4f9767f94a4
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 16:18:27 +0200

    add svgs for the toolbar

---
+++ public/svgs/filter.js
@@ -0,0 +1,368 @@
+export const ShelfSVG = () => {
+  return (
+    <svg
+      fill="none"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 17V7m0 10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2m0 10a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2M9 7a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2m0 10V7m0 10a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2"
+      ></path>
+    </svg>
+  );
+};
+export const CollectionSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"></path>
+    </svg>
+  );
+};
+export const ZoomInSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+      ></path>
+      <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"></path>
+      <path
+        fillRule="evenodd"
+        d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"
+      ></path>
+    </svg>
+  );
+};
+export const ZoomOutSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+      ></path>
+      <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"></path>
+      <path
+        fillRule="evenodd"
+        d="M3 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+      ></path>
+    </svg>
+  );
+};
+export const RecentSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      height="1em"
+      width="1em"
+      overflow="visible"
+    >
+      <path
+        d="m434.8 137.65-149.36-68.1c-16.19-7.4-42.69-7.4-58.88 0L77.3 137.65c-17.6 8-17.6 21.09 0 29.09l148 67.5c16.89 7.7 44.69 7.7 61.58 0l148-67.5c17.52-8 17.52-21.1-.08-29.09ZM160 308.52l-82.7 37.11c-17.6 8-17.6 21.1 0 29.1l148 67.5c16.89 7.69 44.69 7.69 61.58 0l148-67.5c17.6-8 17.6-21.1 0-29.1l-79.94-38.47"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+      <path
+        d="m160 204.48-82.8 37.16c-17.6 8-17.6 21.1 0 29.1l148 67.49c16.89 7.7 44.69 7.7 61.58 0l148-67.49c17.7-8 17.7-21.1.1-29.1L352 204.48"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+    </svg>
+  );
+};
+export const LibrarySVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <rect
+        width="64"
+        height="368"
+        x="32"
+        y="96"
+        rx="16"
+        ry="16"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></rect>
+      <path
+        d="M112 224h128M112 400h128"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+      <rect
+        width="128"
+        height="304"
+        x="112"
+        y="160"
+        rx="16"
+        ry="16"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></rect>
+      <rect
+        width="96"
+        height="416"
+        x="256"
+        y="48"
+        rx="16"
+        ry="16"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></rect>
+      <path
+        d="m422.46 96.11-40.4 4.25c-11.12 1.17-19.18 11.57-17.93 23.1l34.92 321.59c1.26 11.53 11.37 20 22.49 18.84l40.4-4.25c11.12-1.17 19.18-11.57 17.93-23.1L445 115c-1.31-11.58-11.42-20.06-22.54-18.89Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+    </svg>
+  );
+};
+export const ResetSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1024 1024"
+      height="1em"
+      width="1em"
+      overflow="visible"
+    >
+      <path d="m899.1 869.6-53-305.6H864c14.4 0 26-11.6 26-26V346c0-14.4-11.6-26-26-26H618V138c0-14.4-11.6-26-26-26H432c-14.4 0-26 11.6-26 26v182H160c-14.4 0-26 11.6-26 26v192c0 14.4 11.6 26 26 26h17.9l-53 305.6c-.3 1.5-.4 3-.4 4.4 0 14.4 11.6 26 26 26h723c1.5 0 3-.1 4.4-.4 14.2-2.4 23.7-15.9 21.2-30zM204 390h272V182h72v208h272v104H204V390zm468 440V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H416V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H202.8l45.1-260H776l45.1 260H672z"></path>
+    </svg>
+  );
+};
+export const QuoteSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
+      <path d="M7.066 6.76A1.665 1.665 0 0 0 4 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112zm4 0A1.665 1.665 0 0 0 8 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112z"></path>
+    </svg>
+  );
+};
+export const StorySVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path d="M6.94 14.036c-.233.624-.43 1.2-.606 1.783.96-.697 2.101-1.139 3.418-1.304 2.513-.314 4.746-1.973 5.876-4.058l-1.456-1.455 1.413-1.415 1-1.001c.43-.43.915-1.224 1.428-2.368-5.593.867-9.018 4.292-11.074 9.818zM17 9.001 18 10c-1 3-4 6-8 6.5-2.669.334-4.336 2.167-5.002 5.5H3C4 16 6 2 21 2c-1 2.997-1.998 4.996-2.997 5.997L17 9.001z"></path>
+    </svg>
+  );
+};
+export const SummarySVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        d="M368 415.86V72a24.07 24.07 0 0 0-24-24H72a24.07 24.07 0 0 0-24 24v352a40.12 40.12 0 0 0 40 40h328"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+      <path
+        d="M416 464a48 48 0 0 1-48-48V128h72a24 24 0 0 1 24 24v264a48 48 0 0 1-48 48Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+      <path
+        d="M240 128h64M240 192h64M112 256h192M112 320h192M112 384h192"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32px"
+      ></path>
+      <path d="M176 208h-64a16 16 0 0 1-16-16v-64a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v64a16 16 0 0 1-16 16Z"></path>
+    </svg>
+  );
+};
+export const AddBookSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M3 8v11c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19c0-.101.009-.191.024-.273.112-.576.584-.717.988-.727H21V4c0-1.103-.897-2-2-2H6c-1.206 0-3 .799-3 3v3zm3-4h13v12H5V5c0-.806.55-.988 1-1z"></path>
+      <path d="M11 14h2v-3h3V9h-3V6h-2v3H8v2h3z"></path>
+    </svg>
+  );
+};
+export const CurrentBookSVG = () => {
+  return (
+    <svg
+      fill="none"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+      ></path>
+    </svg>
+  );
+};
+export const ArrowRightSVG = () => {
+  return (
+    <svg
+      fill="none"
+      strokeWidth="2"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M5 12h14M12 5l7 7-7 7"></path>
+    </svg>
+  );
+};
+export const FilterSVG = () => {
+  return (
+    <svg
+      fill="none"
+      strokeWidth="2"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M22 3H2l8 9.46V19l4 2v-8.54L22 3z"></path>
+    </svg>
+  );
+};
+export const ShuffleSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path
+        fillRule="evenodd"
+        d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"
+      ></path>
+      <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"></path>
+    </svg>
+  );
+};
+export const ListSVG = () => {
+  return (
+    <svg
+      fill="currentColor"
+      strokeWidth="0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1024 1024"
+      height="16px"
+      width="16px"
+      overflow="visible"
+    >
+      <path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"></path>
+    </svg>
+  );
+};
