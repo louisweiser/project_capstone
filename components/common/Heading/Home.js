5112eceb970f67790b5d1f861d4012e2d9bf60c1
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 10:14:21 +0200

    refactor Heading component for the Homepage

--- components/common/Heading/Home.js
+++
@@ -1,9 +0,0 @@
-import styles from "./home.module.css";
-
-export default function Header({ title }) {
-  return (
-    <header className={styles.header}>
-      <h1 className={styles.font}>{title}</h1>
-    </header>
-  );
-}
---
+++ components/common/Heading/HomePage.js
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import Image from "next/image";
+import styled from "styled-components";
+
+import { MyContext } from "@/contexts/myContext.js";
+
+const StyledHeader = styled.header`
+  /*layout*/
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  /*dimension*/
+  width: 100%;
+  padding: 0 20px;
+  /*style*/
+  background: none;
+  font-size: 30px;
+  border-bottom: 2px solid #03314b;
+`;
+
+const StyledImageContainer = styled.div`
+  /*layout*/
+  position: absolute;
+  bottom: -7px;
+  left: 0;
+  z-index: -1;
+`;
+
+const StyledText = styled.h1`
+  font-family: "Fasthand";
+`;
+
+export default function Header({ title }) {
+  const { screenWidth } = useContext(MyContext);
+
+  const height = Math.floor(screenWidth * 0.40357142857);
+
+  return (
+    <StyledHeader>
+      <StyledText>{title}</StyledText>
+      <StyledImageContainer>
+        <Image
+          src="/images/crop.jpg"
+          width={screenWidth}
+          height={height}
+          alt="img"
+        ></Image>
+      </StyledImageContainer>
+    </StyledHeader>
+  );
+}
