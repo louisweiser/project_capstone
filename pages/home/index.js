2777ec217ba5f0a89b02ea9401ba2a6310e7d764
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 10:16:12 +0200

    refactor Homepage

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,13 +1,13 @@
-import Background from "@/components/common/Background/beige.js";
-import Header from "@/components/common/Heading/Home.js";
-import DailyQuotes from "@/components/pages/home/DailyQuote.js";
+import BackgroundIllustration from "@/components/common/Background/Illustration.js";
+import Header from "@/components/common/Heading/HomePage.js";
+import DailyQuote from "@/components/pages/home/DailyQuote.js";
 
 export default function HomePage() {
   return (
     <>
-      <Background></Background>
+      <BackgroundIllustration color={"#b69b8a"}></BackgroundIllustration>
       <Header title="BookLog"></Header>
-      <DailyQuotes></DailyQuotes>
+      <DailyQuote></DailyQuote>
     </>
   );
 }
