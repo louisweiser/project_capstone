e3812995b559ee722984d66f83de84d01cfdb21a
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 14:28:56 +0200

    implement navigation in home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,7 @@
 import BackgroundIllustration from "@/components/common/Background/Illustration.js";
 import Header from "@/components/common/Heading/HomePage.js";
 import DailyQuote from "@/components/pages/home/DailyQuote.js";
+import Navigation from "@/components/common/Navigation";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <BackgroundIllustration color={"#b69b8a"}></BackgroundIllustration>
       <Header title="BookLog"></Header>
       <DailyQuote></DailyQuote>
+      <Navigation></Navigation>
     </>
   );
 }
