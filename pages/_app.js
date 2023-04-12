bb850c917160c7d21a1790c2e3cef36eb452e07b
Louis Weiser <louis.weiser98@gmail.com>
2023-04-12 11:28:13 +0200

    implement data context in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import "@/assets/fonts.css";
 import { MyProvider } from "../contexts/myContext.js";
+import { DataProvider } from "../contexts/dataContext.js";
 
 export default function App({ Component, pageProps }) {
   return (
     <MyProvider>
-      <Component {...pageProps} />
+      <DataProvider>
+        <Component {...pageProps} />
+      </DataProvider>
     </MyProvider>
   );
 }
