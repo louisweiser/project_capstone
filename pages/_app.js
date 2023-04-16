cad3f3a511daf78365513c6637415abafeca8090
Louis Weiser <louis.weiser98@gmail.com>
2023-04-16 16:57:19 +0200

    configurate vercel analytics in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import "@/assets/fonts.css";
 import { MyProvider } from "../contexts/myContext.js";
 import { DataProvider } from "../contexts/dataContext.js";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <MyProvider>
       <DataProvider>
         <Component {...pageProps} />
+        <Analytics />
       </DataProvider>
     </MyProvider>
   );
